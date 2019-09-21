539c8cf0a0c86384f10e37c320368c43323476b1
greatabel <myreceiver2for2github@gmail.com>
2019-09-21 15:49:42 +0800

    i1check_RDD_type.py

--- i1check_RDD_type.py
+++ i1check_RDD_type.py
@@ -6,6 +6,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark import SQLContext
 from pyspark import SparkConf
 from pyspark import StorageLevel
+from pyspark.rdd import RDD
 from termcolor import colored
 
 
@@ -18,10 +19,10 @@ sc.setLogLevel("WARN")
 
 #创建一个lines的RDD
 inputRDD = sc.textFile('log.txt')
-
+print('inputRDD类型:', inputRDD, isinstance(inputRDD, RDD))
 # 遍历结果
 errorsRDD = inputRDD.filter(lambda line: "error" in line)
-
+print('errorsRDD类型:', errorsRDD, isinstance(errorsRDD, RDD))
 # print(colored('把RDD持久化到内存中', 'red'))
 # pandaslines.persist( StorageLevel.MEMORY_AND_DISK_2 )
 # print('getStorageLevel=', pandaslines.getStorageLevel())
