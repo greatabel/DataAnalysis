f5fc0032cbad8807ecf5a900681dfac27d3f8c54
greatabel <myreceiver2for2github@gmail.com>
2019-09-21 10:19:22 +0800

    i0py_invoke_spark.py

---
+++ i0py_invoke_spark.py
@@ -0,0 +1,28 @@
+# import findspark
+# findspark.init()
+
+from pyspark import SparkContext
+from pyspark import SQLContext
+from pyspark import SparkConf
+
+# import os
+
+# os.environ['PYSPARK_SUBMIT_ARGS'] = "--master local[2] pyspark-shell"
+# os.environ['JAVA_HOME']  = "$(/usr/libexec/java_home -v 13)"
+
+#################### spark调用python文件： ####################
+# spark-submit i0py_invoke_spark.py 
+
+
+
+sc = SparkContext("local","PySparkShell")
+spark = SQLContext(sc)
+
+
+lines = sc.textFile("README.md")
+
+c = lines.count()
+f = lines.first()
+
+print('#'*20)
+print(c, f)
