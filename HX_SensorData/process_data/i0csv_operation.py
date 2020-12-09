e8c12b15f85cdd4d8eb6cc9c71853aa55efda1ea
greatabel <myreceiver2for2github@gmail.com>
2020-12-09 10:13:55 +0800

    process data

---
+++ HX_SensorData/process_data/i0csv_operation.py
@@ -0,0 +1,20 @@
+import os
+import pickle
+import pprint
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import csv
+
+def csv_write(filename, mylist, directory='./'):
+    with open(os.path.join(directory, filename), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(mylist)
+
+
+def csv_reader(filename, directory='./'):
+    with open(os.path.join(directory, filename), newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        mylist = []
+        for row in reader:
+            mylist.append(row[0].split(','))    
+        return mylist
---
+++ HX_SensorData/process_data/i0generate_more_data.py
@@ -0,0 +1,9 @@
+import i0csv_operation
+
+
+def main():
+    ""
+
+
+if __name__ == "__main__":
+    main()
