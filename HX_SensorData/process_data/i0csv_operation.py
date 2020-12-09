8aca8880ce48019f3ae1c38f4214065ea5230d1a
greatabel <myreceiver2for2github@gmail.com>
2020-12-09 17:00:55 +0800

    format py files

--- HX_SensorData/process_data/i0csv_operation.py
+++ HX_SensorData/process_data/i0csv_operation.py
@@ -5,16 +5,17 @@ from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import csv
 
-def csv_write(filename, mylist, mode='w', directory='./'):
-    with open(os.path.join(directory, filename), mode, newline='') as csvfile:
+
+def csv_write(filename, mylist, mode="w", directory="./"):
+    with open(os.path.join(directory, filename), mode, newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(mylist)
 
 
-def csv_reader(filename, directory='./'):
-    with open(os.path.join(directory, filename), newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+def csv_reader(filename, directory="./"):
+    with open(os.path.join(directory, filename), newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=" ", quotechar="|")
         mylist = []
         for row in reader:
-            mylist.append(row[0].split(','))    
+            mylist.append(row[0].split(","))
         return mylist
