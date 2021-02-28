5f6fb7c47870edd808831c529ea65dfe33fedb9e
greatabel <myreceiver2for2github@gmail.com>
2021-02-28 20:15:30 +0800

    finished

--- b9396trafficflow_spark/i5spark_anlysis.py
+++ b9396trafficflow_spark/i5spark_anlysis.py
@@ -21,9 +21,10 @@ def data_anlysis(inputFile):
         input = hiveCtx.read.json(inputFile)
         input.registerTempTable("traffic")
         topTraffics = hiveCtx.sql(
-            "SELECT placeid,car_type_small, car_type_middle, car_type_large, car_total_num, car_speeds FROM traffic ORDER BY time LIMIT 10"
+            "SELECT placeid, time, car_type_small, car_type_middle, car_type_large, car_total_num, car_speeds FROM traffic ORDER BY time LIMIT 10"
         )
-        print("#" * 20, "\n 1. According to lastest time order:", topTraffics.collect(),' record count:', len( topTraffics.collect()))
+        show1 = colored("\n 1. According to lastest time order:", "blue", attrs=['reverse', 'blink'])
+        print("#" * 20, show1, topTraffics.collect(),' record count:', len( topTraffics.collect()))
 
         # https://stackoverflow.com/questions/39535447/attributeerror-dataframe-object-has-no-attribute-map
         topTrafficText = topTraffics.rdd.map(lambda row: row.car_speeds)
@@ -34,8 +35,8 @@ def data_anlysis(inputFile):
             for speed in singlelist:
                 isum += speed
             average_speed = isum / len(singlelist)
-            show = colored("3. total flow is:", "red", attrs=['reverse', 'blink'])
-            print(show, isum, "average spped is:", average_speed)
+            show3 = colored("3. Traffic flow is:", "red", attrs=['reverse', 'blink'])
+            print(show3, isum, "Traffic flow density is:", average_speed)
         time.sleep(3)
 
     sc.stop()
