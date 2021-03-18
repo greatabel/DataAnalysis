ab96e6f9646b2feca0c62ea8c28e5766269231f2
greatabel <myreceiver2for2github@gmail.com>
2021-03-18 16:12:56 +0800

    q1

---
+++ b9876stochastic_simulation/homework3/q1.py
@@ -0,0 +1,25 @@
+'''
+Problem 1. (20 Pts.) Exercise 16 from Chapter 4 of the textbook.
+
+
+Software Made Personal (SMP) customizes software products in two areas: financial tracking and contact management.
+ They currently have a customer support call center that handles technical questions for owners of their software 
+ from the hours of 8 a.m. to 4 p.m. Eastern time.
+When a customer calls, they first listen to a recording that asks them to se- lect among the product lines; 
+historically 59% are financial products and 41% contact management products. 
+The number of customers who can be connected (talking to an agent or on hold) at any one time is essentially unlimited. 
+Each product line has its own agents. If an appropriate agent is available then the call is immediately routed to the agent; 
+if an appropriate agent is not available, then the caller is placed in a hold queue (and listens to a combination of music and ads). 
+SMP has observed that hang-ups very rarely happen.
+SMP is hoping to reduce the total number of agents they need by cross- training agents so that they can answer calls for any product line. 
+Since the agents will not be experts across all products, this is expected to increase the time to process a call by about 5%. 
+The question that SMP has asked you to answer is how many cross-trained agents 
+are needed to provide service at the same level as the current system.
+Incoming calls can be modeled as a Poisson arrival process with a rate of 60 per hour. The mean time required for 
+an agent to answer a question is 5 min, 
+with the actual time being Erlang-2 for financial calls, and Erlang-3 for contact management calls. 
+The current assignment of agents is four for financial and three for contact management. 
+Simulate the system to find out how many agents are needed to deliver the same level of service 
+in the cross-trained system as in the current system.
+
+'''
