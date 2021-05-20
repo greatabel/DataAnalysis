bac390c3eacb40d3769fd8340965f4bda3d3e2a7
greatabel <myreceiver2for2github@gmail.com>
2021-05-20 16:19:52 +0800

    finished i1summarymapper

--- Hadoop Beginner's Guide/ch4mapreduce_programming/i1summarymapper.py
+++ Hadoop Beginner's Guide/ch4mapreduce_programming/i1summarymapper.py
@@ -34,3 +34,13 @@ def main():
  
 if __name__ == "__main__":
 	main()
+
+# hadoop fs -cat /output11/part-00000
+# 2021-05-20 16:19:03,124 WARN util.NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+# badline	326
+# description	61068
+# duration	61068
+# location	61068
+# recorded	61068
+# shape	61068
+# sighted	61068
