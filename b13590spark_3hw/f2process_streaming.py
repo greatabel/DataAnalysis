7703c42169cd05acb43b4926841e81dabe9439ae
greatabel <myreceiver2for2github@gmail.com>
2021-07-27 23:47:45 +0800

    change

--- b13590spark_3hw/f2process_streaming.py
+++ b13590spark_3hw/f2process_streaming.py
@@ -18,23 +18,27 @@ wordCounts.pprint()
 k_v = {}
 
 def result(y):
-	taken = y.take(2)
+	global k_v
+	taken = y.take(6)
 
-	for record in taken[:2]:
+	for record in taken[:6]:
 	    print(record,'#')
 	    if record[0] not in k_v:
 	    	k_v[record[0] ] = record[1]
 
 	print('k_v=', k_v)
-	v_a = 8*8*8 * k_v['a'] / (k_v['a'] + k_v['a_out'] )
-	v_b = 4*4*4  * k_v['a'] / (k_v['a'] + k_v['a_out'] )
-	v_c = 8*8*8 * k_v['a'] / (k_v['a'] + k_v['a_out'] )
-	v_d = v_a + v_b  - v_c
-	print('v_a = ', v_a)
-	print('v_b = ', v_b)
-	print('v_c = ', v_c)
-	print('v_d = ', v_d)
-	return y
+
+		
+	if 'a' in k_v and 'b' in k_v and 'c' in k_v:
+		v_a = 8*8*8 * k_v['a'] / (k_v['a'] + k_v['a_out'] )
+		v_b = 4*4*4  * k_v['b'] / (k_v['b'] + k_v['b_out'] )
+		v_c = 8*8*8 * k_v['c'] / (k_v['a'] + k_v['c_out'] )
+		v_d = v_a + v_b  - v_c
+		print('v_a = ', v_a)
+		print('v_b = ', v_b)
+		print('v_c = ', v_c)
+		print('v_d = ', v_d)
+		return y
 
 rdd = wordCounts.foreachRDD(result)
 
