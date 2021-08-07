ccbcb28cb80280040801f8701ac84643ea000dbf
greatabel <myreceiver2for2github@gmail.com>
2021-08-07 21:42:39 +0800

    run local kafka

--- b13800kafka/i0test_kafka.py
+++ b13800kafka/i0test_kafka.py
@@ -1,11 +1,5 @@
-from time import sleep
-from json import dumps
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
-	value_serializer=lambda x: dumps(x).encode('utf-8'),
-	api_version=(0,11,5))
-
-# http://www.seaxiang.com/blog/QVVJ32
+# start
+# kafka osx: http://www.seaxiang.com/blog/QVVJ32
 # localip: 192.168.0.104
 '''
 start kafka:
@@ -17,14 +11,41 @@ docker run -d --name kafka \
 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.0.104:9092 \
 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 wurstmeister/kafka
 
+
+### create topic ##
+
+bin/kafka-topics.sh --create --zookeeper 192.168.0.104:2181 --replication-factor 1 --partitions 1 --topic mykafka
+
+### producer ##
+
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic mykafka
+
+
+### consumer ##
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic mykafka --from-beginning
+
 '''
 
 # event_logger = KafkaProducer(bootstrap_servers='localhost:9092')
-events_topic = 'events'
+# -*- coding: UTF-8 -*-
+from kafka import KafkaProducer
+import json
+import datetime
 
+topic='myTopic'
+producer = KafkaProducer(bootstrap_servers='192.168.0.104:9092',value_serializer=lambda m:json.dumps(m).encode("utf-8"))  
+# 连接kafka
+# 参数bootstrap_servers：指定kafka连接地址
+# 参数value_serializer：指定序列化的方式，我们定义json来序列化数据，当字典传入kafka时自动转换成bytes
+# 用户密码登入参数
+# security_protocol="SASL_PLAINTEXT"
+# sasl_mechanism="PLAIN"
+# sasl_plain_username="maple"
+# sasl_plain_password="maple"
 
-for j in range(10):
-    print("Iteration", j)
-    data = {'counter': j}
-    producer.send(events_topic, 'default'.encode())
-    sleep(0.5)
+for i in range(1000):
+    data={"num":i,"ts":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+    producer.send(topic,data)
+
+producer.close()
