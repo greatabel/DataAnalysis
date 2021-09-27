f84a10dce3e115e1de19ecb58dff422942f66b58
greatabel <myreceiver2for2github@gmail.com>
2021-09-27 13:01:40 +0800

    add visualization to a14686

--- a14687ca_simulate_transport/car_model.py
+++ a14687ca_simulate_transport/car_model.py
@@ -1,11 +1,14 @@
 import random
 
 
-def init_car_model(cell_temp, leng, cell2, way, auto_p, car_p):
+def init_car_model(cell_temp, leng, cell2, way, tesla_rate, car_p):
+    # 0 代表这个位置是空的，暂时还没有车🚗占据
     if cell_temp[way][0] == 0:
         leng[way] = leng[way] + 1
-        if random.randint(1, 100) <= car_p *(1- auto_p) * 100:
-            cell_temp[way][0] = 1  # people's car
-        elif random.randint(1, 100) <= car_p *auto_p * 100:
-            cell_temp[way][0] = 2  # auto car
+        if random.randint(1, 100) <= car_p * (1 - tesla_rate) * 100:
+            # 燃油车
+            cell_temp[way][0] = 1
+        elif random.randint(1, 100) <= car_p * tesla_rate * 100:
+            # 电动汽车
+            cell_temp[way][0] = 2
     cell2[way][0] = random.randint(0, 2)
--- a14687ca_simulate_transport/main.py
+++ a14687ca_simulate_transport/main.py
@@ -1,87 +1,118 @@
 import numpy as np
 import random
 import copy
-import matplotlib.pyplot as plt
+import time
 
+
+import matplotlib.pyplot as plt
+from termcolor import colored, cprint
 
 from car_model import init_car_model
+from visualization import visulize_to_png, show_img, show_simplify_to_path
 
 
+def custom_map_2d_to_1d(x, width):
+    speedup = 1
+    change_lane_rate = 1
+    cular_scale = 1
+    if 0 < x < width * 1 / 3:
+        speedup = 1
+        change_lane_rate = 1
+        cular_scale = 1
+    elif width / 3 < x <= width / 2:
+        speedup = 2
+        change_lane_rate = 1.5
+        cular_scale = 2
+    else:
+        speedup = 1
+        change_lane_rate = 2
+        cular_scale = 1
+    return speedup, change_lane_rate, cular_scale
 
-def move(way):
-    for j in range(0, width-1):
-        if j==0 and cell[way][0]!=0:
+
+def car_simulate_driving(way, width):
+    for j in range(0, width - 1):
+
+        # 获得当前位置街区的加速因子，换道印子，元胞扩大比例
+        speedup, change_lane_rate, cular_scale = custom_map_2d_to_1d(j, width)
+
+        if j == 0 and cell[way][0] != 0:
             x = 0
-            for i in range(j+1,j+4):
-                if cell[way][i]!=0:
-                    x=1
-            if x==0:
-                cell_temp[way][1]=cell_temp[way][0]
-                cell_temp[way][0]=0
+            for i in range(j + 1, j + 4):
+                if cell[way][i] != 0:
+                    x = 1
+            if x == 0:
+                cell_temp[way][1] = cell_temp[way][0]
+                cell_temp[way][0] = 0
                 cell2[way][1] = cell2[way][0]
                 cell2[way][0] = -1
-        elif cell[way][j]!=0:
+        elif cell[way][j] != 0:
             if cell2[way][j] == way:
                 cell2[way][j] = -1
             if cell2[way][j] != -1 and cell_temp[way][j] == 1:
                 a = change_way(way, j)
-                if a ==0:
+                if a == 0:
                     continue
-            elif cell2[way][j] != -1 and cell_temp[way][j] == 2 and j+5+way>width:
+            elif cell2[way][j] != -1 and cell_temp[way][j] == 2 and j + 5 + way > width:
                 a = change_way(way, j)
                 if a == 0:
                     continue
             x = 0
-            for i in range(j+1,min(j+3,width-1)):
-                if cell[way][i]!=0:
-                    x=1
-            if x==0:
-                if  j+3<width-4 and cell[way][j+3]== 0 and cell_temp[way][j]==1 and random.randint(1,100)<=20:
-                        cell_temp[way][j+2]=cell_temp[way][j]
-                        cell_temp[way][j] = 0
-                        cell2[way][j + 2] = cell2[way][j]
-                        cell2[way][j] = 0
-                        k = j+2
+            for i in range(j + 1, min(j + 3, width - 1)):
+                if cell[way][i] != 0:
+                    x = 1
+            if x == 0:
+                if (
+                    j * cular_scale + 3 < width - 4
+                    and cell[way][j + 3] == 0
+                    and cell_temp[way][j] == 1
+                    and random.randint(1, 100) <= 20
+                ):
+                    cell_temp[way][j + 2] = cell_temp[way][j]
+                    cell_temp[way][j] = 0
+                    cell2[way][j + 2] = cell2[way][j]
+                    cell2[way][j] = 0
+                    k = j + 2 * speedup
                 else:
-                    cell_temp[way][j+1]=cell_temp[way][j]
-                    cell_temp[way][j]=0
+                    cell_temp[way][j + 1] = cell_temp[way][j]
+                    cell_temp[way][j] = 0
                     cell2[way][j + 1] = cell2[way][j]
                     cell2[way][j] = 0
-                    k=min(j+1,width-2)
-                if cell_temp[way][k]==1:
-                    if random.randint(1,100)<=5 and cell_temp[way][k-1]==0:
-                        cell_temp[way][k-1]=1
-                        cell_temp[way][k]=0
+                    k = min(j + 1, width - 2)
+                if cell_temp[way][k] == 1:
+                    if random.randint(1, 100) <= 5 and cell_temp[way][k - 1] == 0:
+                        cell_temp[way][k - 1] = 1
+                        cell_temp[way][k] = 0
                         cell2[way][k - 1] = cell2[way][k]
                         cell2[way][k] = -1
-                    elif random.randint(1,100)<=5 and cell_temp[way][k+1]==0:
+                    elif random.randint(1, 100) <= 5 and cell_temp[way][k + 1] == 0:
                         cell_temp[way][k + 1] = 1
                         cell_temp[way][k] = 0
                         cell2[way][k + 1] = cell2[way][k]
                         cell2[way][k] = -1
 
 
-def remove(way):
-    if cell_temp[way][width-1] != 0 and cell2[way][width-1]==-1:
-        leng[3] = leng[3]+1
-        leng[way] = leng[way] -1
-        cell_temp[way][width-1]=0
+def off_road(way):
+    if cell_temp[way][width - 1] != 0 and cell2[way][width - 1] == -1:
+        leng[3] = leng[3] + 1
+        leng[way] = leng[way] - 1
+        cell_temp[way][width - 1] = 0
 
 
 def change_way(way, local):
     m = 0
     n = 0
-    if way ==1:
+    if way == 1:
         if cell2[way][local] != way:
-            for i in range(local-3,min(local+4,width-1)):
+            for i in range(local - 3, min(local + 4, width - 1)):
                 if cell[cell2[way][local]][i] != 0:
                     m = 1
             if m == 0:
-                cell_temp[cell2[way][local]][local+1]=cell_temp[way][local]
+                cell_temp[cell2[way][local]][local + 1] = cell_temp[way][local]
                 cell_temp[way][local] = 0
-                leng[way]=leng[way]-1
-                leng[cell2[way][local]] = leng[cell2[way][local]]+1
-                cell2[cell2[way][local]][local+1] = -1
+                leng[way] = leng[way] - 1
+                leng[cell2[way][local]] = leng[cell2[way][local]] + 1
+                cell2[cell2[way][local]][local + 1] = -1
                 cell2[cell2[way][local]][local] = 0
         if m == 0:
             return 0
@@ -89,14 +120,14 @@ def change_way(way, local):
             return 1
     if way == 0:
         if cell2[way][local] != way:
-            for i in range(local-3,min(local+4,width-1)):
+            for i in range(local - 3, min(local + 4, width - 1)):
                 if cell[1][i] != 0:
                     m = 1
             if m == 0:
-                cell_temp[1][local+1]=cell_temp[way][local]
+                cell_temp[1][local + 1] = cell_temp[way][local]
                 cell_temp[way][local] = 0
-                leng[way]=leng[way]-1
-                leng[1] = leng[1]+1
+                leng[way] = leng[way] - 1
+                leng[1] = leng[1] + 1
                 cell2[1][local + 1] = cell2[way][local]
                 cell2[way][local] = 0
         if m == 0:
@@ -105,7 +136,7 @@ def change_way(way, local):
             return 1
     if way == 2:
         if cell2[way][local] != way:
-            for i in range(local - 3, min(local + 4,width-1)):
+            for i in range(local - 3, min(local + 4, width - 1)):
                 if cell[1][i] != 0:
                     m = 1
             if m == 0:
@@ -121,53 +152,77 @@ def change_way(way, local):
             return 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    show_img("resources/geographical_urban.jpg")
+
+    time.sleep(1)
+
+    show_simplify_to_path()
+
     length = 3
-    width = 200
+    width = 30
+
     d = 0
-    cell = np.zeros((length,width),int)
+    cell = np.zeros((length, width), int)
     cell_temp = copy.deepcopy(cell)
     cell2 = copy.deepcopy(cell)
     for i in range(0, length):
-        for j in range(0,width):
+        for j in range(0, width):
             cell_temp[i][j] = 0
     for i in range(0, length):
-        for j in range(0,width):
+        for j in range(0, width):
             cell2[i][j] = -1
-    #cell_temp[i][1] = 1
-    #cell_temp[i][2] = 1
+    # cell_temp[i][1] = 1
+    # cell_temp[i][2] = 1
     global leng
     leng = {0: 0, 1: 0, 2: 0, 3: 0}
     list1 = []
     # for i in range(0, 100):
     for i in range(0, 5):
-        print('i', '='*10, i)
+        welcome = colored(
+            "#" * 10 + " This turn of car_simulate_driving: " + str(i) + "#" * 10,
+            "red",
+            attrs=["reverse", "blink"],
+        )
+        print(welcome, "\n")
+        # print("i", "=" * 10, i)
         while True:
             if d % 4 == 0:
-                init_car_model(cell_temp, leng, cell2,  0, i/100, 0.3)
+                init_car_model(cell_temp, leng, cell2, 0, i / 10, 0.3)
+                # init_car_model(cell_temp, leng, cell2,  0, i/100, 0.3)
             if d % 3 == 0:
-                init_car_model(cell_temp, leng, cell2, 1, i/100, 0.3)
+                init_car_model(cell_temp, leng, cell2, 1, i / 10, 0.3)
             if d % 5 == 0:
-                init_car_model(cell_temp, leng, cell2, 2, i/100, 0.3)
+                init_car_model(cell_temp, leng, cell2, 2, i / 10, 0.3)
             cell = copy.deepcopy(cell_temp)
             # 显示每一轮的图像
+            # plt.imshow(cell, aspect='auto')
             plt.imshow(cell)
             plt.pause(0.0000000001)
-            move(0)
-            move(1)
-            move(2)
-            remove(0)
-            remove(1)
-            remove(2)
+            car_simulate_driving(0, width)
+            car_simulate_driving(1, width)
+            car_simulate_driving(2, width)
+            off_road(0)
+            off_road(1)
+            off_road(2)
             d = d + 1
             # if leng[3] >= 2000:
-            if leng[3] >= 5:
+            if leng[3] >= 10:
                 list1.append(d)
-                d=0
-                leng[3]=0
+                d = 0
+                leng[3] = 0
                 break
-            print('len(list1)=', len(list1))
-            print('leng[3]=', leng[3])
+            print("len(list1)=", len(list1))
+            print("leng[3]=", leng[3])
+            msg = colored(
+                "#" * 10
+                + " The accumulate car_simulate_driving lan: "
+                + str(d)
+                + "#" * 10,
+                "blue",
+                attrs=["reverse", "blink"],
+            )
+            print(msg, "\n")
 
     print(list1)
 
@@ -175,12 +230,7 @@ if __name__ == '__main__':
     # plt.pause(0.2)
 
     x = np.arange(0, 1, 0.2)
+    print("x=", x)
     # x = np.arange(0, 1, 0.01)
-
-    plt.close()
-
-    plt.plot(x, list1)
-    plt.savefig('result_comparison.png')
-    plt.show()
-    # plt.pause(5)
-
+    y = list1
+    visulize_to_png(x, y)
