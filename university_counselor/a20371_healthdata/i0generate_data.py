6953efebd47ae5a96a5d5618bf9c7519eee50b63
greatabel <myreceiver2for2github@gmail.com>
2022-10-15 17:27:47 +0800

    alter i1

--- university_counselor/a20371_healthdata/i0generate_data.py
+++ university_counselor/a20371_healthdata/i0generate_data.py
@@ -4,25 +4,43 @@ import random
 
 def main():
     # 年龄,BMI,饮酒 ,心脏病,糖尿病
-    first_row = ['age','bmi','drink','heart_disease','diabetes']
+    first_row = ['age','bmi','drink','heart_disease','diabetes','patient_id', 'sex', 'painloc', 'painexer', 'relrest']
 
     rows = []
     rows.append(first_row)
-    for i in range(1000000*5):
+    patient_id = 1
+    for i in range(1000000*3):
+    # for i in range(1000*5):
         age = random.randint(60, 90)
         if age > 80:
-            age = age  - 10
+            age = age  - random.randint(0, 10)
         bmi = random.randint(10, 30)
+        if bmi > 15:
+            bmi += random.randint(0, 3)
+            age += random.randint(0, 10)
         drink = random.randint(0, 1)
         heart_disease = random.randint(0, 1)
         diabetes = random.randint(0, 1)
+
+        patient_id += 1
+        sex = 0
+        if random.randint(0, 100) > 52:
+            sex = 0
+        else:
+            sex = 1
+
+        painloc = random.randint(0, 1)
+        painexer = random.randint(0, 1)
+        relrest = random.randint(0, 1)
+
         if age > 70:
             if random.randint(0, 1) == 0 and drink == 1:
                 heart_disease = 1
         if age > 80:
             if random.randint(0, 1) == 0 and drink == 1:
                 diabetes = 1
-        row = [age, bmi, drink, heart_disease, diabetes ]
+        
+        row = [age, bmi, drink, heart_disease, diabetes, patient_id, sex, painloc, painexer, relrest]
         rows.append(row)
 
     print('len(rows)=',len(rows))
