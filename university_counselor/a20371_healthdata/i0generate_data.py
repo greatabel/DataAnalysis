1779cf4951d8f8ea5b3d18e5dd310b8e5db6d20a
greatabel <myreceiver2for2github@gmail.com>
2022-12-02 09:45:34 +0800

    change i1spark_ml

--- university_counselor/a20371_healthdata/i0generate_data.py
+++ university_counselor/a20371_healthdata/i0generate_data.py
@@ -5,12 +5,12 @@ import random
 def main():
     # 年龄,BMI,饮酒 ,心脏病,糖尿病
     first_row = ['age','bmi','drink','heart_disease','diabetes','patient_id', 'sex', 'painloc', 'painexer', 'relrest']
-    # first_row = ["barometric_value","humidity","ultraviolet_rays","rain","clound","record_id","pm1","pm2","pm3","pm4"]
+    # first_row = ["barometric_value","humidity","ultraviolet_rays","average_quality","ozone_relative","record_id","pm1","pm2","pm3","pm4"]
 
     rows = []
     rows.append(first_row)
     patient_id = 1
-    for i in range(1000000*3):
+    for i in range(1000000*4):
     # for i in range(1000*5):
         age = random.randint(60, 90)
         if age > 80:
