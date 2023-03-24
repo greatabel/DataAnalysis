a427137907134486890f68332f3b5f8a2e7bab42
greatabel <myreceiver2for2github@gmail.com>
2023-03-24 19:37:56 +0800

    a25667

--- a25667_pca/i2add_iterate_algorithmII.py
+++ a25667_pca/i2add_iterate_algorithmII.py
@@ -47,43 +47,34 @@ def local_gaussian_mechanism_high_dimension(data, epsilon, delta, rho, k):
     else:
         print(k,s,p, "==> satisfy the condition k ≤ s ≤ p")
 
-    # 使用 PCA 主成分分析
-    pca = PCA(n_components=k)
-    pca.fit(cov_matrix)
-    # 主成分是带噪声数据的最佳线性投影
-    principal_components = pca.components_.T
+    X = np.zeros_like(cov_matrix)
+    Y = np.zeros_like(cov_matrix)
+    U = np.zeros_like(cov_matrix)
 
-    return principal_components
-
-# def local_gaussian_mechanism_high_dimension(data, epsilon, delta, rho, k):
-#     # 计算噪声标准偏差
-#     sigma = np.sqrt(2 * np.log(1.25 / delta))
-
-#     # 生成高斯噪音
-#     p, n = data.shape
-#     noise = np.random.normal(loc=0, scale=sigma, size=(p, p, n))
-#     for i in range(n):
-#         noise[:, :, i] = np.triu(noise[:, :, i]) + np.triu(noise[:, :, i], 1).T
-
-#     # 给数据添加噪音
-#     noisy_data = data + noise
-
-#     # 计算协方差矩阵
-#     cov_matrix = np.zeros((p, p))
-#     for i in range(n):
-#         cov_matrix += np.einsum(
-#             "ij, kj -> ik", noisy_data[:, :, i], noisy_data[:, :, i]
-#         )
-#     cov_matrix /= n
-
-#     # 使用 PCA 主成分分析
-#     pca = PCA(n_components=k)
-#     pca.fit(cov_matrix)
-#     # 主成分是带噪声数据的最佳线性投影
-#     principal_components = pca.components_.T
+    # 增加迭代部分
+    max_iter = 5000
+    tol = 1e-5  
+    # Iterate until convergence or max_iter reached
+    for t in range(max_iter):
+        X_prev = X.copy()
 
 
-#     return principal_components
+        X = np.linalg.inv(np.eye(p) + rho * np.eye(p)) @ (Y - U + rho * cov_matrix)
+
+
+        Y = soft_thresholding_operator(X + U, 1 / rho)
+
+
+        U = U + X - Y
+        
+        diff_norm = np.linalg.norm(X - X_prev)
+        if diff_norm < tol:
+            print(f"Converged at iteration {t+1}, diff_norm: {diff_norm:.6f}")
+            break
+        else:
+            print(f"Iteration {t+1}, diff_norm: {diff_norm:.6f}")
+
+    return Y
 
 
 def load_local_covtype_data(local_folder):
@@ -100,35 +91,22 @@ def load_local_covtype_data(local_folder):
 # covtype = fetch_covtype()
 # X, y = covtype.data, covtype.target
 
-# 加载 covtype 数据集
+# Load the COVERTYPE dataset
 covtype = fetch_covtype()
-
-# X, _ = covtype.data, covtype.target
-local_data_folder = "covertype"
-
-try:
-    # 尝试加载 COVERTYPE 数据集
-    covtype = fetch_covtype()
-    print("----from internent---")
-    X, y = covtype.data, covtype.target
-except Exception as e:
-    print("无法下载 COVERTYPE 数据集，尝试从本地加载数据集。")
-    X, y = load_local_covtype_data(local_data_folder)
+X, y = covtype.data, covtype.target
 
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-# 买家自己定义数据集使用量（上次他要求）
 n_samples = 1000
 indices = np.random.choice(X.shape[0], n_samples, replace=False)
 X_sampled = X[indices].T
 
-# 设置选项，买家可以自己调整这块
 epsilon = 1.0
 delta = 1e-5
 rho = 0.1
 
-# 测试不同k
+# Test different k
 print("\n\n")
 print("----1.测试不同k和方差 ----")
 
@@ -151,8 +129,7 @@ plt.title("Variance vs. k")
 plt.grid()
 plt.show()
 
-# 随着选择更多的主成分，累积数据方差会逐渐增加
-
+# Compare original and privacy-protected data
 print("\n\n")
 print("----2.选择一种k=2情况比较原始数据和隐私化后数据分类----")
 
@@ -175,9 +152,4 @@ plt.legend()
 plt.title("First 2 Principal Components of Original and Privacy-Protected Data")
 plt.show()
 
-
 print("\n\n")
-
-
-print("\n\n")
-
