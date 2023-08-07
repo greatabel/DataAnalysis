b5bf03c2b0d814d83eb24b013b0a1d323f505ca9
greatabel <myreceiver2for2github@gmail.com>
2023-08-07 20:45:43 +0800

    mv to common repository

--- university_counselor/a12057_spark/i0Analytics/data_visualization/data.js
+++
@@ -1,4 +0,0 @@
-var DATA = {
-'data2020':52, 'data2019':23,'total_sentiment_polarity':0.0, 'average_sentiment_polarity':0.0,'num_positive':0, 'num_neural':101,'num_nagtive':0, 'data2019':23 };
-var RELATED_WORDS = { 'python':5, 'book':3};
-var WHO_TWEETS = {'Biswadip jana':6,'AMTI':4,'panda_ben 🐡🐡':3,'Antiwar.com':3,'Oceanner':2,'Devdiscourse':2,'Paul':2,'Bloomberg Politics':2, };
--- university_counselor/b9876stochastic_simulation/course/readme.md
+++
@@ -1,9 +0,0 @@
-create virtual environment:
-python3 -m venv  simulation-env
-
-then enter virtual environment:
-Windows run:
-simulation-env\Scripts\activate.bat
-
-Unix/MacOS run:
-source simulation-env/bin/activate
